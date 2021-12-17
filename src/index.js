7d3e5b7eba722379e6b1128631eb6dba90ecf134
AleksandrVasilko <avasilko7@gmail.com>
2021-12-18 01:02:28 +0200

    Add masag in async/await

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/main.scss';
 import './js/search-images.js'
+import './css/style.css'
 
 
 
--- src/js/search-images.js
+++ src/js/search-images.js
@@ -1,9 +1,13 @@
-
+import { Notify } from 'notiflix/build/notiflix-aio';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 import galleryCardsTpl from '../templates/gallery-card.hbs'
 
 
 
 const API_KEY = '24801124-06fccb0e586a8b9d373fb9ce7';
+const imagesPerPage = 40;
+
 const search = document.querySelector('.button');
 
 const refs = {
@@ -32,12 +36,12 @@ const onSearchSubmit = (e) => {
 
 const onLoadMoreImages = () => {
     refs.pageNumber += 1
-    getImages();
+    searchFunction();
 }
 
 
 refs.form.addEventListener('submit', onSearchSubmit)
-//refs.loadMoreButton.addEventListener('click', onLoadMoreImages)
+refs.loadMoreButton.addEventListener('click', onLoadMoreImages)
 
 
 search.addEventListener('click', searchFunction);
@@ -45,19 +49,52 @@ search.addEventListener('click', searchFunction);
 async function searchFunction() {
     //console.log('Приветggg');
     try {
-        const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${refs.search}&image_type=photo&orientation=horizontal&safesearch=true&page=${refs.pageNumber}&per_page=40`);
+        const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${refs.search}&image_type=photo&orientation=horizontal&safesearch=true&page=${refs.pageNumber}&per_page=${imagesPerPage}`);
         const images = await response.json()
 
+        if (refs.totalQuantityOfImages > images.totalHits - 1) {
+            console.log('Попал')
+            Notify.failure("We're sorry, but you've reached the end of search results.")
+            refs.loadMoreButton.classList.add('is-hidden')
+            return;
+        }
+
+        if (images.hits.length === 0) {
+            Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            refs.loadMoreButton.classList.add('is-hidden')
+            Loading.remove()
+            return
+        }
+
+        refs.totalHits = images.totalHits
+        if (refs.pageNumber === 1) {
+            Notify.success(`Hooray! We found ${refs.totalHits} images.`);
+            
+        }
+
+
+        console.log(refs.totalQuantityOfImages)
+        console.log(images.totalHits)
         const markup = images.hits.map(galleryCardsTpl).join('')
         refs.galleryOfImages.insertAdjacentHTML('beforeend', markup)
         lightbox.refresh();
         refs.loadMoreButton.classList.remove('is-hidden')
-        refs.totalQuantityOfImages += imagesPerPage
+        if ((refs.totalQuantityOfImages += imagesPerPage) > (images.totalHits - imagesPerPage)) {
+            refs.totalQuantityOfImages += imagesPerPage - 1;
+        }
+        //refs.totalQuantityOfImages += imagesPerPage
+        
     }
-
     
-
     catch (error) {
         console.log(error.message);
     }
 }
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+   disableRightClick: true,
+    scrollZoom: false,
+    captionDelay: 250,
+    captionsData: 'alt', 
+});   
